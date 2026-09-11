cc6e20185aa75c33b40d42631d5cf9bac1e7dd9a
isikjon <isikjandev@gmail.com>
2026-09-11 12:13:39 +0500

    Правки 11.09: рамка меню фильтров, расширение строки при мультивыборе, стрелка с главной на всех баннерах, фон фильтров Вдохновения во всю ширину, зум баннера не наезжает на текст

--- wp-content/themes/okoyom/template-parts/static/about.php
+++ wp-content/themes/okoyom/template-parts/static/about.php
@@ -12,13 +12,11 @@ defined( 'ABSPATH' ) || exit;
             </h1>
         </div>
     </div>
-    <div class="container">
-        <div class="downArrowCard">
-            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
-                <path d="M3.5 5.25L7 8.75L10.5 5.25" stroke="white" stroke-opacity="0.4" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-        </div>
-    </div>
+    <span class="mural-hero__scroll" aria-hidden="true">
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M6 9l6 6 6-6" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    </span>
 </section>
 <section class="sectionMain">
     <div class="container">
